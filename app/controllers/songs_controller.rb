d7eb5a2448fc00b01fc028d24f346ca2a9978b8c
Michael Anderson <michanders@msn.com>
2018-02-03 20:37:39 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,8 +14,8 @@ class SongsController < ApplicationController
 
   def show
     if params[:artist_id]
-      @artist = Artist.find_by(id: params[:artist_id])
-      @song = @artist.songs.find_by(id: params[:id])
+      @artist = Artist.find_by_id(params[:artist_id])
+      @song = @artist.songs.find_by_id(params[:id])
       if @song.nil?
         redirect_to artist_songs_path(@artist), alert: "Song not found"
       end
@@ -26,6 +26,12 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    if params[:artist_id] && Artist.find_by_id(params[:artist_id])
+      @nested_artist = true
+      @song.artist = @artist if @artist = Artist.find_by_id(params[:artist_id])
+    elsif params[:artist_id] && !Artist.find_by_id(params[:artist_id])
+      redirect_to artists_path
+    end
   end
 
   def create
@@ -39,7 +45,14 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    @song = Song.find_by_id(params[:id])
+    @artist = Artist.find_by_id(params[:artist_id])
+    @nested_artist = true if params[:artist_id]
+    if !@song
+      redirect_to artist_songs_path(@artist)
+    elsif params[:artist_id] && @song.artist != @artist
+      redirect_to artists_path
+    end
   end
 
   def update
@@ -64,7 +77,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
